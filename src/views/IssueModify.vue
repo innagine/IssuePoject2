7618676051039cdf52a95d347a013c3940c6fb9a
RianHola <1204957089@qq.com>
2020-11-19 14:36:51 +0800

    新建 IssueModify.vue

---
+++ src/views/IssueModify.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="IssueModify">
+    <div class="Modify">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="修改 Issue" prop="desc">
+          <el-input type="textarea" rows=10 v-model="ruleForm.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >立即创建</el-button
+          >
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+          <el-button @click="resetForm('ruleForm')">返回</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        desc: "",
+      },
+      rules: {
+        desc: [{ required: true, message: "请修改Issue", trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
+
+<style>
+    .Modify{
+        margin: 20px 20px;
+    }
+</style>
